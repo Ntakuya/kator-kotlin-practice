18bae5497f890114c270a57d094e9d338ea3f7ff
Gma_Gama <nagahama.takuya@outlook.jp>
2022-09-05 12:13:37 +0900

    feat(ktor/creatting-a-new-ktor-project) add new ktor project

---
+++ ktor/learn/creating-a-new-ktor-project/app/src/main/kotlin/app/plugins/Routing.kt
@@ -0,0 +1,15 @@
+package app.plugins
+
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+
+fun Application.configureRouting() {
+  routing {
+    get("/") {
+      call.respondText("Hello World!BBBBBBB AAA")
+    }
+  }
+}

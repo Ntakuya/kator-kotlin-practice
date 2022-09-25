35558bcbf437428efa77b8b5e0c8c57818a6964a
Gma_Gama <nagahama.takuya@outlook.jp>
2022-09-26 07:29:19 +0900

    feat(ktor/docs) add ktor docs

--- ktor/ktor-server/server-websockets/app/src/main/kotlin/app/App.kt
+++ ktor/ktor-server/server-websockets/app/src/main/kotlin/app/App.kt
@@ -1,5 +1,6 @@
 package app
 
+import app.plugins.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
@@ -9,6 +10,7 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 
 fun Application.module() {
+     Serializer()
      routing {
          get("/") {
 	     call.respondText("Server")
